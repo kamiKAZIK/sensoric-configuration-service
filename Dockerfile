4cc2b703301a00f7cf66fa2ad9fade7b82d41097
Eduardas Kazakas <eduardas.kazakas@gmail.com>
2018-04-08 21:55:34 +0200

    Added proper entrypoint

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,12 @@ ENV ARTIFACT_NAME sensoric-configuration-service.jar
 RUN mkdir -p /usr/local/applications \
 	&& apk add --update \
 		tomcat-native \
+		bash \
 	&& rm -rf /var/cache/apk/*
 WORKDIR /usr/local/applications
 COPY ${JAR_FILE} ${ARTIFACT_NAME}
+COPY docker-entrypoint.sh /usr/local/bin
+
+ENTRYPOINT ["docker-entrypoint.sh"]
+
 EXPOSE 38888
-ENTRYPOINT java $JAVA_OPTS -jar $ARTIFACT_NAME \
-    --encrypt.key-store.password="$(cat $KEYSTORE_PASSWORD_FILE)" \
-    --encrypt.key-store.secret="$(cat $KEYSTORE_SECRET_FILE)" \
-    $@
---
+++ docker-entrypoint.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -e
+
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+file_env 'KEYSTORE_PASSWORD'
+if [ -z "$KEYSTORE_PASSWORD" ]; then
+    echo >&2 '  You need to specify KEYSTORE_PASSWORD'
+    exit 1
+else
+    set -- "--encrypt.key-store.password=$KEYSTORE_PASSWORD" "$@"
+fi
+
+file_env 'KEYSTORE_SECRET'
+if [ -z "KEYSTORE_SECRET" ]; then
+    echo >&2 '  You need to specify KEYSTORE_SECRET'
+    exit 1
+else
+    set -- "--encrypt.key-store.secret=$KEYSTORE_SECRET" "$@"
+fi
+
+set -- java $JAVA_OPTS -jar $ARTIFACT_NAME "$@"
+
+exec "$@"
