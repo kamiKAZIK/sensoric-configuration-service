aeaf8bf8150cb404a5d604b5fdd9200cd1c21f66
Eduardas Kazakas <eduardas.kazakas@gmail.com>
2019-01-01 10:11:08 +0100

    Small refactoring, format changes

---
+++ src/main/java/com/sensoric/configuration/security/SecurityProperties.java
@@ -0,0 +1,26 @@
+package com.sensoric.configuration.security;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "sensoric.security")
+class SecurityProperties {
+	private UserProperties user, manager;
+
+	List<UserProperties> getUsers() {
+		return Arrays.asList(user, manager);
+	}
+
+	@Getter
+	@Setter
+	static final class UserProperties {
+		private String name, password;
+		private String[] roles;
+	}
+}
